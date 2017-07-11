c2e0a11242c3802c38151eb433ced6abda707e49
zhangfeibufei <zhangran1789@163.com>
2017-07-11 18:43:15 +0800

    构造函数，无new构造方法
    不写new直接生成对象，new写在了一个return语句中。构造函数也是一个立即执行函数，体现了高内聚低耦合

--- MyJquery.js
+++ MyJquery.js
@@ -7,28 +7,39 @@
     "use strict";
     var _MyJquery = window.MyJquery,_$ = window.$; //把可能存在的同名变量保存起来
 
-    // var MyJquery = window.MyJquery = window.$ = function (selector,context) {
-    //     return new MyJquery.fn.init(selector,context);
-    // };
-    var MyJquery = {};
-    MyJquery.sayhello = function () {
-        console.log("hello myJquery!");
-    };
-    
+
+    //初始化MyJquery类
+    //无new 构造
+    var MyJquery =(function() {
+        var MyJquery = function (selector) {
+            return new MyJquery.fn.init(selector);
+        };
+        MyJquery.fn= MyJquery.prototype;
+        MyJquery.fn.init = function (selector)
+        {
+            var elem;
+            elem = document.getElementById(selector);
+            this.elem = elem;
+        };
+        return MyJquery;
+    })();
+    // MyJquery.say = "hey,boy";
+
+    //简写prototype
+    // MyJquery.fn = MyJquery.prototype;
+
+
+    //构造函数
+    // MyJquery
+
+
+    //解决命名空间冲突，让出$使用权
     MyJquery.noConflict = function () {
         window.$ = _$;
         return MyJquery;
     };
-    // MyJquery.extend = MyJquery.fn = MyJquery.prototype;
-    //
-    // MyJquery.extend({
-    //     noConflict:function (deep) {
-    //         window.$ = _$;//放回去
-    //         if(deep)
-    //             window.MyJquery = _MyJquery;
-    //         return MyJquery;
-    //     }
-    // })
+
+    //取得的命名空间加入window对象
     window.MyJquery = window.$ = MyJquery;
 
 })(window);
